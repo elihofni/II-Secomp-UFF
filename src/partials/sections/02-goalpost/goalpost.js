938a710d4cddab8e7605453b3aaf2e2e09f05db6
Elihofni Lima <elihofni.lima@globoesporte.com>
2018-10-17 01:39:31 -0300

    rollback to begin

--- src/partials/sections/02-goalpost/goalpost.js
+++ src/partials/sections/02-goalpost/goalpost.js
@@ -1,37 +1,22 @@
-const FORWARD = 1;
-const BACKWARD = -1;
+const FORWARD = 1; // eslint-disable-line
+const BACKWARD = -1; // eslint-disable-line
 
-let currentFrame;
-let totalFrames;
+let currentFrame;  // eslint-disable-line
+let totalFrames; // eslint-disable-line
 
 export default class Goalpost {
   static init() {
+    // Inicializa os contadores
     currentFrame = 0;
     totalFrames = $('.penalty-shadow-wrapper').length - 1;
 
-    // Ativa o primeiro placar e sombra
-    $('.gamescores > .gamescore-wrapper:first-child').addClass('gamescore-wrapper--active');
-    $('.penalty-shadows > .penalty-shadow-wrapper:first-child').addClass('penalty-shadow-wrapper--active');
-
-    // Inicializa os cliques de navegação
-    $('.penalties-shadow-nav.penalties-shadow-nav--left').on('click', () => this.navToPenalty(currentFrame, BACKWARD));
-    $('.penalties-shadow-nav.penalties-shadow-nav--right').on('click', () => this.navToPenalty(currentFrame, FORWARD));
+    // TODO Ativar o primeiro placar e sombra
+    // TODO Inicializar os cliques de navegação
   }
 
-  static navToPenalty(frame, direction) {
-    const isForwardingToNull = (frame === totalFrames && direction === FORWARD);
-    const isBackwardingToNull = (frame === 1 && direction === BACKWARD);
-
-    if (!isBackwardingToNull && !isForwardingToNull) {
-      // Atualiza a sombra
-      $('.penalty-shadow-wrapper.penalty-shadow-wrapper--active').removeClass('penalty-shadow-wrapper--active');
-      $(`#penalty-shadow-wrapper--${frame + direction}`).addClass('penalty-shadow-wrapper--active');
-
-      // Atualiza o placar
-      $('.gamescore-wrapper.gamescore-wrapper--active').removeClass('gamescore-wrapper--active');
-      $(`#gamescore-wrapper--${frame + direction}`).addClass('gamescore-wrapper--active');
-
-      currentFrame = frame + direction;
-    }
+  static navToFrame(frame, direction) { // eslint-disable-line
+    // TODO Verificar se está tentando navegar para além dos limites
+    // TODO Atualizar a sombra
+    // TODO Atualizar o placar
   }
 }
